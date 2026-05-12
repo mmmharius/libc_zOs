2524e03965efcab29c653f3d3a28fa72f670cb00
mmmharius <mariuspapin@rcmm.fr>
2026-05-13 01:59:54 +0200

    ADD: all .c only strcmp/len/cpy asm

---
+++ ft_atoi.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign   = 1;
+	result = 0;
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			sign = -1;
+	while (*str >= '0' && *str <= '9')
+		result = result * 10 + (*str++ - '0');
+	return (sign * result);
+}
